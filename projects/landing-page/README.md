954392255bee3856654ec6ea0b6c7caa41cb5b93
Dennis Frankenbach <dennisfrankenbach@gmail.com>
2021-09-20 15:59:19 +0200

    feat: add project requirements

--- projects/landing-page/README.md
+++ projects/landing-page/README.md
@@ -1,13 +1,10 @@
 # Landing Page Project
 
-## Table of Contents
-
-* [Instructions](#instructions)
-
-## Instructions
-
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
-
-To get started, open `js/app.js` and start building out the app's functionality
-
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## Description
+This project consists of Javascript code, that influences the appearance and behavior of a website.
+E.g. it has
+* Eventlisteners
+* navigation bar is built dynamically
+* smooth scrolling
+* mark active sections
+* navigation bar is highlighting the last link, that was clicked
--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -11,19 +11,22 @@
  * 
  * JS Standard: ESlint
  * 
-*/
+ */
 
 /**
  * Define Global Variables
  * 
-*/
+ */
+const navbar = document.getElementById("navbar__list");
+const bottomButton = document.getElementById("bottom__button");
+const sections = document.querySelectorAll("section");
 
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
-*/
+ */
 
 
 
@@ -31,12 +34,53 @@
  * End Helper Functions
  * Begin Main Functions
  * 
-*/
+ */
 
 // build the nav
 
+// source of how to add elements to the list: https://stackoverflow.com/questions/4310759/updating-an-html-list-using-js
+// source of how to use dataset: https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
+const navBuilder = () => {
+    const fragment = document.createDocumentFragment();
+    sections.forEach(section => {
+        const liElement = document.createElement('li');
+        liElement.innerHTML = `<a class="menu__link" href="#${section.id}">${section.dataset.nav}</a>`;
+        fragment.appendChild(liElement);
+    });
+    navbar.appendChild(fragment);
+}
+
+navBuilder();
 
 // Add class 'active' to section when near top of viewport
+const updateActiveClass = (section, isInScreen) => {
+    if (isInScreen) {
+        section.classList.add("your-active-class");
+    } else {
+        section.classList.remove("your-active-class");
+    }
+};
+
+// source of how get the position: https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element-relative-to-the-browser-window
+const position = (element) => {
+    return element.getBoundingClientRect().top;
+};
+
+const isInScreen = (position) => {
+    return -200 <= position && position <= 200;
+};
+
+const updateSections = () => {
+    sections.forEach(section => {
+        const pos = position(section);
+        updateActiveClass(
+            section,
+            isInScreen(pos)
+        );
+    });
+};
+
+
 
 
 // Scroll to anchor ID using scrollTO event
@@ -46,7 +90,7 @@
  * End Main Functions
  * Begin Events
  * 
-*/
+ */
 
 // Build menu 
 
@@ -55,3 +99,52 @@
 // Set sections as active
 
 
+const isOnBottom = () => {
+
+    const ft = document.getElementsByClassName("page__footer")[0]
+    return ft.getBoundingClientRect().bottom <= 1100;
+
+};
+
+// source of how to update innerHTML: https://stackoverflow.com/questions/4310759/updating-an-html-list-using-js
+const bottomChecker = () => {
+    if (isOnBottom()) {
+        bottomButton.innerHTML = "<button class='section-button' onclick='jumpToTop()'>Scroll to top</button>";
+    } else {
+        bottomButton.innerHTML = "";
+    }
+
+};
+
+const jumpToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+
+window.addEventListener("scroll", bottomChecker);
+window.addEventListener("scroll", updateSections);
+// Set sections as active
+
+
+// source for smoothScrolling: https://stackoverflow.com/questions/7717527/smooth-scrolling-when-clicking-an-anchor-link
+const smoothScrolling = () => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+            updateNavbar(anchor);
+        });
+    });
+}
+
+smoothScrolling();
+
+const updateNavbar = (current_anchor) => {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.classList.remove("your-active-state");
+    });
+    current_anchor.classList.add("your-active-state");
+}
